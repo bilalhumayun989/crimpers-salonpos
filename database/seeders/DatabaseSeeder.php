e98f63ad736e5158dd03a31d005f901cb36bd1cf
safullah <safullahzafar@gmail.com>
2026-04-25 02:57:35 +0500

    Initial commit

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -29,39 +29,11 @@ class DatabaseSeeder extends Seeder
             'role' => 'admin',
         ]);
 
-        // Employee
+        // Employee (Optional fallback account)
         User::updateOrCreate(['email' => 'user@gmail.com'], [
             'name' => 'Staff Member',
             'password' => bcrypt('12345678'),
             'role' => 'employee',
-        ]);
-
-        // Categories
-        $hair = Category::updateOrCreate(['slug' => 'hair-services'], ['name' => 'Hair Services', 'type' => 'service']);
-        $skin = Category::updateOrCreate(['slug' => 'skin-care'], ['name' => 'Skin Care', 'type' => 'service']);
-        $products = Category::updateOrCreate(['slug' => 'retail-products'], ['name' => 'Retail Products', 'type' => 'product']);
-
-        // Services
-        Service::updateOrCreate(['name' => 'Men\'s Haircut'], [
-            'category_id' => $hair->id,
-            'price' => 25.00,
-            'duration' => 30,
-            'is_popular' => true
-        ]);
-        
-        Service::updateOrCreate(['name' => 'Express Facial'], [
-            'category_id' => $skin->id,
-            'price' => 45.00,
-            'duration' => 45,
-            'is_popular' => true
-        ]);
-
-        // Products
-        Product::updateOrCreate(['barcode' => '123456789'], [
-            'category_id' => $products->id,
-            'name' => 'Herbal Shampoo 250ml',
-            'price' => 15.00,
-            'stock' => 50,
         ]);
     }
 }
