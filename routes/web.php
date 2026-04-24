1b3d26d4d6e5e514e42025951881ed984dd3526a
safullah <safullahzafar@gmail.com>
2026-04-25 00:05:13 +0500

    Initial commit

--- routes/web.php
+++ routes/web.php
@@ -50,9 +50,9 @@ Route::middleware(['auth'])->group(function () {
     Route::put('/branches/{branch}/hours', [BranchController::class, 'updateHours'])->name('branches.update-hours')->middleware('permission:business,view');
 
     // Two Factor routes
-    Route::get('/verify', [TwoFactorController::class, 'index'])->name('verify.index');
-    Route::post('/verify', [TwoFactorController::class, 'verify'])->name('verify.store');
-    Route::get('/verify/resend', [TwoFactorController::class, 'resend'])->name('verify.resend');
+    // Route::get('/verify', [TwoFactorController::class, 'index'])->name('verify.index');
+    // Route::post('/verify', [TwoFactorController::class, 'verify'])->name('verify.store');
+    // Route::get('/verify/resend', [TwoFactorController::class, 'resend'])->name('verify.resend');
 
     // Dashboard
     Route::middleware(['permission:dashboard,view'])->group(function() {
